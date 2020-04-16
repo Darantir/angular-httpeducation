adb40a8e51a5d0eace521b3c622f00296a9eb531
Darantir <yakushev_1996@inbox.ru>
2020-04-16 18:25:47 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
-import { SearchResultComponent } from './search-result/search-result.component';
+
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, SimpleHttpComponent, SearchResultComponent ],
+  declarations: [ AppComponent, HelloComponent, SimpleHttpComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
