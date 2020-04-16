94492f173b13aa44361beec414b22e7575c37fd7
Darantir <yakushev_1996@inbox.ru>
2020-04-16 19:33:57 +0200

    1 file modified

--- src/app/you-tu-be-search/you-tu-be-search.injectable.ts
+++ src/app/you-tu-be-search/you-tu-be-search.injectable.ts
@@ -1,10 +1,10 @@
 import {
-YouTubeSearchService,
+YoutubeSearchServiceService,
  YOUTUBE_API_KEY,
  YOUTUBE_API_URL
  } from './youtube-search-service.service';
 
  export const youTubeSearchInjectables: Array<any> = [
- {provide: YouTubeSearchService, useClass: YouTubeSearchService},
+ {provide: YoutubeSearchServiceService, useClass: YoutubeSearchServiceService},
  {provide: YOUTUBE_API_KEY, useValue: YOUTUBE_API_KEY},
  {provide: YOUTUBE_API_URL, useValue: YOUTUBE_API_URL} ];
