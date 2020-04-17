c4ccfea8cd89aa6e83ee401b78eab59e91b5de77
Darantir <yakushev_1996@inbox.ru>
2020-04-17 16:30:21 +0200

    1 file modified

--- src/app/you-tu-be-search/you-tube-search.service.ts
+++ src/app/you-tu-be-search/you-tube-search.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Inject,  } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SearchResult} from './search-result.model';
-import {Observable} from 'rxjs/Rx';
+import {Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
 export const YOUTUBE_API_KEY = "XXX_YOUR_KEY_HERE_XXX";
 export const YOUTUBE_API_URL: string = "https://www.googleapis.com/youtube/v3/search";
 
@@ -19,13 +20,24 @@ export class YouTubeSearchService {
   }
 
   search(query: string): Observable<SearchResult[]> {
-   const params: string = [
-   `q=${query}`,
-   `key=${this.apiKey}`,
-   `part=snippet`,
-   `type=video`,
-   `maxResults=10`
-  ].join('&');
-   const queryUrl = `${this.apiUrl}?${params}`
-  }
+ const params: string = [
+ `q=${query}`,
+ `key=${this.apiKey}`,
+ `part=snippet`,
+ `type=video`, `maxResults=10`
+ ].join('&');
+ const queryUrl = `${this.apiUrl}?${params}`;
+ return this.http.get(queryUrl).map(response => {
+ return <any>response['items'].map(item => {
+ // console.log("raw item", item); // uncomment if you want to debug
+ return new SearchResult({
+ id: item.id.videoId,
+ title: item.snippet.title,
+ description: item.snippet.description,
+ thumbnailUrl: item.snippet.thumbnails.high.url
+ });
+ });
+ });
+ }
+
 }
