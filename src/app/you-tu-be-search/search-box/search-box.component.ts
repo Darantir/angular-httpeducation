c21abdd68305869c0e703503f31134b516c604a3
Darantir <yakushev_1996@inbox.ru>
2020-04-17 17:42:18 +0200

    1 file modified

--- src/app/you-tu-be-search/search-box/search-box.component.ts
+++ src/app/you-tu-be-search/search-box/search-box.component.ts
@@ -3,6 +3,7 @@ import { SearchResult} from './src/app/you-tu-be-search/search-result.model';
 import { YouTubeSearchService } from '../you-tube-search.service';
 import {Observable} from 'rxjs'
 
+ 
 
 @Component({
   selector: 'app-search-box',
@@ -16,7 +17,29 @@ export class SearchBoxComponent implements OnInit {
   private el: ElementRef) { }
 
   ngOnInit(): void {
-    Observable.fromEvent(this.el.nativeElement, 'keyup')
+  // convert the `keyup` event into an observable stream
+  Observable.fromEvent(this.el.nativeElement, 'keyup')
+  .map((e: any) => e.target.value) // extract the value of the input
+  .filter((text: string) => text.length > 1) // filter out if empty
+  .debounceTime(250) // only once every 250ms
+  .do(() => this.loading.emit(true)) // enable loading
+  // search, discarding old events if new input comes in
+  .map((query: string) => this.youtube.search(query))
+  .switch()
+  // act on the return of the search
+  .subscribe(
+    (results: SearchResult[]) => { // on sucesss
+ this.loading.emit(false);
+ this.results.emit(results);
+ },
+ (err: any) => { // on error
+ console.log(err);
+ this.loading.emit(false);
+ },
+ () => { // on completion
+   this.loading.emit(false);
+   }
+   );
   }
 
 }
