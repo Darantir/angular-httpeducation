3a0cf78c5b3728d05e4b3b166902145d8d47e97a
Darantir <yakushev_1996@inbox.ru>
2020-04-17 17:07:42 +0200

    1 file modified

--- src/app/you-tu-be-search/search-box/search-box.component.ts
+++ src/app/you-tu-be-search/search-box/search-box.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,Input,Output, EventEmitter } from '@angular/core';
-import { SearchResult} from '../you-tu-be-search/search-result.model';
+import { SearchResult} from './src/app/you-tu-be-search/search-result.model';
 
 
 @Component({
