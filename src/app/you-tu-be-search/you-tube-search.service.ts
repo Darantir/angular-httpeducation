dd38f443c260fcb602b47b0acb8ea4ded3a56a00
Darantir <yakushev_1996@inbox.ru>
2020-04-17 16:15:44 +0200

    1 file modified

--- src/app/you-tu-be-search/you-tube-search.service.ts
+++ src/app/you-tu-be-search/you-tube-search.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject,  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SearchResult} from './search-result.model';
+import {Observable} from 'rxjs/Rx';
 export const YOUTUBE_API_KEY = "XXX_YOUR_KEY_HERE_XXX";
 export const YOUTUBE_API_URL: string = "https://www.googleapis.com/youtube/v3/search";
 
@@ -8,9 +11,21 @@ export const YOUTUBE_API_URL: string = "https://www.googleapis.com/youtube/v3/se
 
 export class YouTubeSearchService {
 
-  constructor() 
+  constructor(private http: HttpClient, 
+  @Inject(YOUTUBE_API_KEY) private apiKey:string,
+  @Inject(YOUTUBE_API_URL) private apiUrl:string ) 
   {
    
   }
 
+  search(query: string): Observable<SearchResult[]> {
+   const params: string = [
+   `q=${query}`,
+   `key=${this.apiKey}`,
+   `part=snippet`,
+   `type=video`,
+   `maxResults=10`
+  ].join('&');
+   const queryUrl = `${this.apiUrl}?${params}`
+  }
 }
