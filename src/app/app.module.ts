4bd671c8201fc4af5d527d30964897052b22a1eb
Darantir <yakushev_1996@inbox.ru>
2020-04-17 16:53:52 +0200

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,13 @@ import { SimpleHttpComponent } from './simple-http/simple-http.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { YouTubeSearchService } from './you-tu-be-search/you-tube-search.service';
 import {youTubeSearchInjectables} from './you-tu-be-search/you-tu-be-search.injectable';
+import { SearchBoxComponent } from './you-tu-be-search/search-box/search-box.component';
 
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, SimpleHttpComponent ],
+  declarations: [ AppComponent, HelloComponent, SimpleHttpComponent, SearchBoxComponent ],
   bootstrap:    [ AppComponent ],
   providers: [YouTubeSearchService]
 })
